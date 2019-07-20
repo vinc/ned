b0b179f80a97756efed09038c135916c8e7a23df
Vincent Ollivier <v@vinc.cc>
2019-07-21 00:45:34 +0200

    Add more dragons

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,56 @@ fn read(path: &str) -> Vec<String> {
     data.lines().map(|l| l.to_string()).collect()
 }
 
+fn print_error(error: &str, show_verbose: bool) {
+    if show_verbose {
+        println!("{}", format!("? {}", error).red());
+    } else {
+        println!("{}", "?".red());
+    }
+}
+
+fn print_line(line: &str, i: usize, n: usize, show_number: bool) {
+    if show_number {
+        let width = n.to_string().len();
+        let number = format!("{:>width$}", i, width = width).green();
+        println!("{} {}", number, line);
+    } else {
+        println!("{}", line);
+    }
+}
+
 fn main() {
     let mut insert_mode = false;
     let mut file = None;
     let mut addr = 0;
     let mut text = Vec::new();
+    let mut prompt = "";
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    let mut show_debug = false;
+    let mut show_verbose = false;
+    let args: Vec<String> = env::args().filter(|arg| {
+        if arg == "--debug" {
+            show_debug = true;
+        }
+        if arg == "--verbose" {
+            show_verbose = true;
+            prompt = "> ";
+        }
+        !arg.starts_with("--")
+    }).collect();
+
+    if args.len() > 1 {
         file = Some(args[1].clone());
         text = read(&args[1]);
         addr = text.len();
     }
 
-    let re = Regex::new(r"(?P<addr1>\d*)(?P<sep>[,;]?)(?P<addr2>\d*)(?P<cmd>[a-z]*) ?(?P<params>\w*)").unwrap();
+    let re = Regex::new(r"(?P<addr1>\d*)(?P<range_sep>[,;%]?)(?P<addr2>\d*)(?P<cmd>[a-z]*)(?P<cmd_sep>[ /]?)(?P<params>.*)").unwrap();
 
     loop {
         let mut input = String::new();
         if !insert_mode {
-            print!(":");
+            print!("{}", prompt);
         }
         io::stdout().flush().ok();
         io::stdin().read_line(&mut input).expect("Unable to read stdin");
@@ -49,7 +80,9 @@ fn main() {
 
 
         let caps = re.captures(input.trim()).unwrap();
-        println!("# regex: {:?}", caps);
+        if show_debug {
+            println!("# regex: {:?}", caps);
+        }
 
         let cmd = &caps["cmd"];
 
@@ -57,38 +90,51 @@ fn main() {
             break;
         }
 
-        let params: Vec<&str> = caps["params"].split_whitespace().collect();
+        let cmd_sep = if &caps["cmd_sep"] == "/" { "/" } else { " " };
+        let params: Vec<&str> = caps["params"].split(cmd_sep).collect();
 
-        // Range
-        let mut begin = addr;
-        let mut end = addr;
+        let mut begin = match &caps["range_sep"] {
+            "," | "%" => 1,
+            _         => addr
+        };
 
-        if caps["addr1"].len() > 0 {
-            begin = caps["addr1"].parse::<usize>().unwrap();
-            end = begin;
-        } else if &caps["sep"] == "," {
-            begin = 1;
-            end = text.len();
-        }
-        if caps["addr2"].len() > 0 {
-            end = caps["addr2"].parse::<usize>().unwrap();
-        }
+        begin = match &caps["addr1"] {
+            ""  => begin,
+            "." => addr,
+            "$" => text.len(),
+            _   => caps["addr1"].parse::<usize>().unwrap()
+        };
+
+        let mut end = match &caps["range_sep"] {
+            "," | "%" => text.len(),
+            _         => begin
+        };
+
+        end = match &caps["addr2"] {
+            ""  => end,
+            "." => addr,
+            "$" => text.len(),
+            _   => caps["addr2"].parse::<usize>().unwrap()
+        };
+
         addr = end;
 
         if begin > end || end > text.len() {
-            println!("?");
+            print_error("Invalid range", show_verbose);
             continue;
         }
 
         let range = begin .. end + 1;
-        println!("# range: [{},{}]", begin, end);
-        println!("# addr: {}", addr);
-        println!("# cmd: {}", cmd);
-        println!("# params: {:?}", params);
+        if show_debug {
+            println!("# range: [{},{}]", begin, end);
+            println!("# addr: {}", addr);
+            println!("# cmd: {}", cmd);
+            println!("# params: {:?}", params);
+        }
 
         match cmd {
             "" => {
-                println!("{}", text[addr - 1]);
+                print_error("Invalid command", show_verbose);
             },
             "a" => { // Add line after addr
                 insert_mode = true;
@@ -103,41 +149,67 @@ fn main() {
                 text.remove(addr - 1);
             },
             "o" => {
-                let f = params[1];
+                let f = params[0];
                 file = Some(String::from(f));
-                text = read(params[1]);
+                text = read(params[0]);
             },
-            "w" => {
-                let path = if params.len() == 2 {
-                    String::from(params[1])
+            "w" | "wq" => {
+                let path = if params.len() == 1 {
+                    String::from(params[0])
                 } else if let Some(f) = file.clone() {
                     f
                 } else {
-                    println!("No file name");
+                    print_error("No file name", show_verbose);
                     continue;
                 };
                 let data = text.join("\n");
                 fs::write(path, data).expect("Unable to write file");
-            },
-            "p" => {
-                for i in range {
-                    println!("{}", &text[i - 1]);
+                if cmd == "wq" {
+                    break;
                 }
             },
-            "n" | "pn" => {
-                let width = text.len().to_string().len();
+            "p" |"n" | "pn" => {
+                if range.start == 0 {
+                    print_error("Invalid range", show_verbose);
+                    continue;
+                }
+                let n = text.len();
+                let show_number = cmd.ends_with("n");
                 for i in range {
-                    let num = format!("{:>width$}", i, width = width).green();
-                    let cur = if i == addr {
-                        "*".green()
-                    } else {
-                        " ".normal()
-                    };
-                    println!("{}{} {}", cur, num, &text[i - 1]);
+                    print_line(&text[i - 1], i, n, show_number);
+                    addr = i;
+                }
+            },
+            "g" | "gn" => {
+                if range.start == 0 {
+                    print_error("Invalid range", show_verbose);
+                    continue;
+                }
+                let re = Regex::new(params[0]).unwrap();
+                let n = text.len();
+                let show_number = cmd.ends_with("n");
+                for i in range {
+                    if re.is_match(&text[i - 1]) {
+                        print_line(&text[i - 1], i, n, show_number);
+                        addr = i;
+                    }
+                }
+            },
+            "s" => {
+                if range.start == 0 {
+                    print_error("Invalid range", show_verbose);
+                    continue;
+                }
+                let re = Regex::new(params[0]).unwrap();
+                for i in range {
+                    if re.is_match(&text[i - 1]) {
+                        text[i - 1] = re.replace_all(&text[i - 1], params[1]).to_string();
+                        addr = i;
+                    }
                 }
             },
             _ => {
-                println!("?");
+                print_error("Invalid command", show_verbose);
             }
         }
     }
