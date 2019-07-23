f409ea761a09c6516812100377bfe38edac29840
Vincent Ollivier <v@vinc.cc>
2019-07-23 10:30:25 +0200

    Add EditorState struct

--- src/main.rs
+++ src/main.rs
@@ -37,26 +37,37 @@ fn print_line(line: &str, i: usize, n: usize, show_number: bool) {
     }
 }
 
+struct EditorState {
+    dirty: bool,
+    show_help: bool,
+    show_debug: bool,
+    insert_mode: bool,
+    addr: usize,
+    lines: Vec<String>,
+    filename: Option<String>
+}
+
 fn main() {
     let home = std::env::var("HOME").unwrap();
     let history = format!("{}/.ned_history", home);
 
-    // State
-    let mut dirty = false;
-    let mut insert_mode = false;
-    let mut filename = None;
-    let mut lines = Vec::new();
-    let mut addr = 0;
+    let mut ed = EditorState {
+        dirty: false,
+        show_debug: false,
+        show_help: true,
+        insert_mode: false,
+        filename: None,
+        lines: Vec::new(),
+        addr: 0
+    };
 
     let mut prompt = "> ";
-    let mut show_help = true;
-    let mut show_debug = false;
     let args: Vec<String> = env::args().filter(|arg| {
         if arg == "--debug" {
-            show_debug = true;
+            ed.show_debug = true;
         }
         if arg == "--silent" {
-            show_help = false;
+            ed.show_help = false;
             prompt = "";
         }
         !arg.starts_with("--")
@@ -64,9 +75,9 @@ fn main() {
 
     if args.len() > 1 {
         let f = args[1].clone();
-        lines = read_lines(&f);
-        addr = lines.len();
-        filename = Some(String::from(f));
+        ed.lines = read_lines(&f);
+        ed.addr = ed.lines.len();
+        ed.filename = Some(String::from(f));
     }
 
     let re = Regex::new(concat!(
@@ -82,7 +93,7 @@ fn main() {
     rl.load_history(&history).ok();
 
     loop {
-        let readline = rl.readline(if insert_mode { "" } else { prompt });
+        let readline = rl.readline(if ed.insert_mode { "" } else { prompt });
         match readline {
             Err(ReadlineError::Interrupted) => {
                 break
@@ -97,13 +108,13 @@ fn main() {
             Ok(input) => {
                 let input = input.trim();
 
-                if insert_mode {
+                if ed.insert_mode {
                     if input == "." {
-                        insert_mode = false;
+                        ed.insert_mode = false;
                     } else {
-                        lines.insert(addr, input.to_string());
-                        addr += 1;
-                        dirty = true;
+                        ed.lines.insert(ed.addr, input.to_string());
+                        ed.addr += 1;
+                        ed.dirty = true;
                     }
                     continue;
                 } else {
@@ -118,103 +129,103 @@ fn main() {
 
                 let mut addr_1 = match &caps["addr_sep"] {
                     "," | "%" => 1,
-                    _         => addr
+                    _         => ed.addr
                 };
 
                 addr_1 = match &caps["addr_1"] {
                     ""  => addr_1,
-                    "." => addr,
-                    "$" => lines.len(),
+                    "." => ed.addr,
+                    "$" => ed.lines.len(),
                     _   => caps["addr_1"].parse::<usize>().unwrap()
                 };
 
                 let mut addr_2 = match &caps["addr_sep"] {
-                    "," | "%" => lines.len(),
+                    "," | "%" => ed.lines.len(),
                     _         => addr_1
                 };
 
                 addr_2 = match &caps["addr_2"] {
                     ""  => addr_2,
-                    "." => addr,
-                    "$" => lines.len(),
+                    "." => ed.addr,
+                    "$" => ed.lines.len(),
                     _   => caps["addr_2"].parse::<usize>().unwrap()
                 };
 
-                addr = addr_2;
+                ed.addr = addr_2;
 
-                if addr_1 > addr_2 || addr_2 > lines.len() {
+                if addr_1 > addr_2 || addr_2 > ed.lines.len() {
                     if addr_1 != 0 || cmd != "a" || !cmd.to_lowercase().ends_with("q") {
-                        print_error(INVALID_RANGE_ERROR, show_help);
+                        print_error(INVALID_RANGE_ERROR, ed.show_help);
                         continue;
                     }
                 }
 
-                if show_debug {
+                if ed.show_debug {
                     println!("# range: [{},{}]", addr_1, addr_2);
-                    println!("# addr: {}", addr);
+                    println!("# addr: {}", ed.addr);
                     println!("# cmd: {}", cmd);
                     println!("# params: {:?}", params);
                 }
 
                 match cmd {
                     "a" => { // Append command
-                        insert_mode = true;
+                        ed.insert_mode = true;
                     },
                     "i" => { // Insert command
-                        insert_mode = true;
-                        if addr > 0 {
-                            addr -= 1;
+                        ed.insert_mode = true;
+                        if ed.addr > 0 {
+                            ed.addr -= 1;
                         }
                     },
                     "c" => { // Change command
                         let range = addr_1 - 1 .. addr_2;
-                        lines.drain(range);
-                        addr = addr_1 - 1;
-                        dirty = true;
-                        insert_mode = true;
+                        ed.lines.drain(range);
+                        ed.addr = addr_1 - 1;
+                        ed.dirty = true;
+                        ed.insert_mode = true;
                     },
                     "d" => { // Delete command
                         let range = addr_1 - 1 .. addr_2;
-                        lines.drain(range);
-                        dirty = true;
-                        addr = addr_1 - 1;
+                        ed.lines.drain(range);
+                        ed.dirty = true;
+                        ed.addr = addr_1 - 1;
                     },
                     "e" => { // Edit command
                         let f = params[0];
-                        lines = read_lines(&f);
-                        addr = lines.len();
-                        filename = Some(String::from(f));
-                        dirty = false;
+                        ed.lines = read_lines(&f);
+                        ed.addr = ed.lines.len();
+                        ed.filename = Some(String::from(f));
+                        ed.dirty = false;
                     },
                     "f" => { // Filename command
                         if params[0] != "" {
-                            filename = Some(String::from(params[0]));
-                        } else if let Some(f) = filename.clone() {
+                            ed.filename = Some(String::from(params[0]));
+                        } else if let Some(f) = ed.filename.clone() {
                             println!("{}", f);
                         } else {
-                            print_error(NO_FILENAME_ERROR, show_help);
+                            print_error(NO_FILENAME_ERROR, ed.show_help);
                         }
                     },
                     "w" | "wq" => { // Write command
                         if params[0] != "" {
-                            filename = Some(String::from(params[0]));
+                            ed.filename = Some(String::from(params[0]));
                         }
-                        if let Some(f) = filename.clone() {
-                            let data = lines.join("\n");
+                        if let Some(f) = ed.filename.clone() {
+                            let data = ed.lines.join("\n");
                             fs::write(f, data).expect("Unable to write file");
-                            dirty = false;
+                            ed.dirty = false;
                         } else {
-                            print_error(NO_FILENAME_ERROR, show_help);
+                            print_error(NO_FILENAME_ERROR, ed.show_help);
                             continue;
                         }
                     },
                     "p" |"n" | "pn" => { // Print and number commands
                         let range = addr_1 .. addr_2 + 1;
-                        let n = lines.len();
+                        let n = ed.lines.len();
                         let show_number = cmd.ends_with("n");
                         for i in range {
-                            print_line(&lines[i - 1], i, n, show_number);
-                            addr = i;
+                            print_line(&ed.lines[i - 1], i, n, show_number);
+                            ed.addr = i;
                         }
                     },
                     "g" => { // Global command
@@ -224,19 +235,19 @@ fn main() {
                         let mut i = addr_1;
                         let mut n = addr_2;
                         while i <= n {
-                            if re.is_match(&lines[i - 1]) {
+                            if re.is_match(&ed.lines[i - 1]) {
                                 match cmd_list {
                                     "p" | "pn" => {
-                                        print_line(&lines[i - 1], i, lines.len(), show_number);
+                                        print_line(&ed.lines[i - 1], i, ed.lines.len(), show_number);
                                     },
                                     "d" => {
-                                        lines.remove(i - 1);
+                                        ed.lines.remove(i - 1);
                                         i -= 1;
                                         n -= 1;
                                     },
                                     _ => {}
                                 }
-                                addr = i;
+                                ed.addr = i;
                             }
                             i += 1;
                         }
@@ -245,16 +256,16 @@ fn main() {
                         let range = addr_1 .. addr_2 + 1;
                         let re = Regex::new(params[0]).unwrap();
                         for i in range {
-                            if re.is_match(&lines[i - 1]) {
-                                lines[i - 1] = re.replace_all(&lines[i - 1], params[1]).to_string();
-                                addr = i;
-                                dirty = true;
+                            if re.is_match(&ed.lines[i - 1]) {
+                                ed.lines[i - 1] = re.replace_all(&ed.lines[i - 1], params[1]).to_string();
+                                ed.addr = i;
+                                ed.dirty = true;
                             }
                         }
                     },
                     "q" => { // Quit command
-                        if dirty {
-                            print_error(DIRTY_ERROR, show_help);
+                        if ed.dirty {
+                            print_error(DIRTY_ERROR, ed.show_help);
                             continue;
                         }
                     },
@@ -262,7 +273,7 @@ fn main() {
                         // Nothing to do here
                     },
                     _ => {
-                        print_error(INVALID_COMMAND_ERROR, show_help);
+                        print_error(INVALID_COMMAND_ERROR, ed.show_help);
                     }
                 }
 
